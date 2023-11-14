da9c2cbaa9be64d3ac5451cd3dae28d39ad774c3
abdallah moumni <abdelrebel001@gmail.com>
2023-11-13 16:38:40 -0800

    commit num 7

---
+++ print_num_oct.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * prinnoct - prints an unsigned integer in octal format.
+ * @arguments: The unsigned integer to print.
+ * @buf: The buffer pointer.
+ * @ibuf: The index for the buffer pointer.
+ *
+ * Return: The number of characters printed.
+ */
+int prinnoct(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int input, i, isnega, c, frst_dig;
+	char *oct, *bin;
+
+	input = va_arg(arguments, int);
+	isnega = 0;
+	if (input == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
+	if (input < 0)
+	{
+		input = (input * -1) - 1;
+		isnega = 1;
+	}
+	ibuf = handl_buf(buf, '0', ibuf);
+	bin = malloc(sizeof(char) * (32 + 1));
+	bin = fill_bin_array(bin, input, isnega, 32);
+	oct = malloc(sizeof(char) * (11 + 1));
+	oct = fill_oct_array(bin, oct);
+	for (frst_dig = i = c = 0; oct[i]; i++)
+	{
+		if (oct[i] != '0' && frst_dig == 0)
+			frst_dig = 1;
+		if (frst_dig)
+		{
+			ibuf = handl_buf(buf, oct[i], ibuf);
+			c++;
+		}
+	}
+	free(bin);
+	free(oct);
+	return (c + 1);
+}
---
+++ print_num_upx.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * prinnupx - prints an unsigned integer in uppercase hexadecimal format.
+ * @arguments: The unsigned integer to print.
+ * @buf: The buffer pointer.
+ * @ibuf: The index for the buffer pointer.
+ *
+ * Return: The number of characters printed.
+ */
+int prinnupx(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int input, i, isnega, c, frst_dig;
+	char *hexa, *bin;
+
+	input = va_arg(arguments, int);
+	isnega = 0;
+	if (input == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
+	if (input < 0)
+	{
+		input = (input * -1) - 1;
+		isnega = 1;
+	}
+	ibuf = handl_buf(buf, '0', ibuf);
+	ibuf = handl_buf(buf, 'X', ibuf);
+	bin = malloc(sizeof(char) * (32 + 1));
+	bin = fill_bin_array(bin, input, isnega, 32);
+	hexa = malloc(sizeof(char) * (8 + 1));
+	hexa = fill_hex_array(bin, hexa, 1, 8);
+	for (frst_dig = i = c = 0; hexa[i]; i++)
+	{
+		if (hexa[i] != '0' && frst_dig == 0)
+			frst_dig = 1;
+		if (frst_dig)
+		{
+			ibuf = handl_buf(buf, hexa[i], ibuf);
+			c++;
+		}
+	}
+	free(bin);
+	free(hexa);
+	return (c + 2);
+}
